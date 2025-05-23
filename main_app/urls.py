c5eaa3104f648bc87dab36c0a5cbd5fabb6cbce2
pankajshinde5057 <pankajshinde5057@gmail.com>
2025-05-23 16:15:17 +0530

    added todays clocked in feature functionality in manager side

--- main_app/urls.py
+++ main_app/urls.py
@@ -134,7 +134,7 @@ urlpatterns = [
      path('manager/leave-reject/<int:leave_id>/', manager_views.reject_leave_request,
           name='reject_leave_request'),
      path('notification/read/<int:notification_id>/<str:type>/<str:role>/', mark_notification_read, name='mark_notification_read'),
-
+     path('manager-todays-attendance/', manager_views.manager_todays_attendance, name='manager_todays_attendance'),
 
      path('get_asset_categories/', manager_views.get_asset_categories, name='get_asset_categories'),
      path('get_available_assets/', manager_views.get_available_assets, name='get_available_assets'),
