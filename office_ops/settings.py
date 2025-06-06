e832792df8a6f0eb52007ef2eff438d5181fba63
pankajshinde5057 <pankajshinde5057@gmail.com>
2025-06-06 12:28:45 +0530

    changes in main setting (DB)

--- office_ops/settings.py
+++ office_ops/settings.py
@@ -92,15 +92,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'koli_ems_db',
-        'USER': config.dbusername,
-        'PASSWORD': config.dbpassword,
-        'HOST': config.dbhost,
-        'PORT': config.db_port,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
