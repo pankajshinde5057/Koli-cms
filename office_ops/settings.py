87f799254b0f71158844b1b79d5bfd7d3611ab57
pankajshinde5057 <priyank.mali.5057@gmail.com>
2025-06-06 12:25:04 +0530

    --

--- office_ops/settings.py
+++ office_ops/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from . import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,12 +91,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'koli_ems_db',
-        'USER': config.dbusername,
-        'PASSWORD': config.dbpassword,
-        'HOST': config.dbhost,
-        'PORT': config.db_port,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
